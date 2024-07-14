7abc3b822ad319ae5f80831428429580d2b0dd27
kai336 <shiturukai.336@gmail.com>
2024-07-14 14:26:43 +0900

    add frontend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,23 @@
-//ここにサーバーの動作を記述
+//app.jsにサーバーの動作を記述
+const express = require('express'); // express のインポート
+const app = express(); // インスタンス化
+const PORT = 5000; // サーバーのポート番号
+
+// ルートエンドポイントの設定
+// get, post, delete, putなどHTTPリクエストで使うメソッドがある
+// HTTPリクエストとは https://qiita.com/minateru/items/8693538bbd0768855266
+// localhost:5000/ にアクセスしたときの動作
+app.get('/', (req, res) => {
+  res.send('Hello World!'); // GETリクエストに対するレスポンスとして文章を送信している
+});
+
+// 他のエンドポイントの設定
+// localhost:5000/about にアクセスしたときの動作
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+// サーバーの起動
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
