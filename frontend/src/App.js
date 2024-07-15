c72703f3eced271b807bfccab4b0acf0d4e7e7ec
otk1-0430 <klsm9853@gmail.com>
2024-07-15 18:47:04 +0900

    app.js/index.html_register/profile.js/signup.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
 import logo from './img/logo.svg';
 import './css/App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './SignUp';
+import Profile from './Profile';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
 
 function App() {
   return (
