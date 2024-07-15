1b3353e2f9328d790c7c933f74dbb90c151903f8
otk1-0430 <klsm9853@gmail.com>
2024-07-15 22:02:38 +0900

    delete my-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-import logo from './img/logo.svg';
-import './css/App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp';
@@ -13,27 +11,6 @@ function App() {
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
