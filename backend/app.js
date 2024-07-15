f52fb7cb93b7cda3a8e1a8f15503ea2b4c450cc1
kai336 <shiturukai.336@gmail.com>
2024-07-15 23:53:56 +0900

    すべてのバグを解決

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 
 //app.jsにサーバーの動作を記述
 const express = require('express'); // express のインポート
-const Pool = require('pg'); // sqlと接続するためのパッケージpgのインポート
+const { Pool } = require('pg'); // sqlと接続するためのパッケージpgのインポート
 const cors = require('cors') // 異なるドメインからのリクエストを許可するために使用
 const bodyParser = require('body-parser'); // body-parserのインポート//HTTPリクエストのボディを解析するために使用
 
@@ -29,7 +29,7 @@ const pool = new Pool({
 
 
 // ユーザー登録エンドポイント
-app.post('/register', async (req, res) => {
+app.post('/api/register', async (req, res) => {
   const { username, password } = req.body;
 
   try {
