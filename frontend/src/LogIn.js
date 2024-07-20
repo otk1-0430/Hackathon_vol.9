58e7f052f78b10e2c1dbd2812fcbd5f94b3eaf7e
otk1-0430 <klsm9853@gmail.com>
2024-07-20 15:13:12 +0900

    LogIn画面作成/ルーター追加/SignUp画面の修正

---
+++ frontend/src/LogIn.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function LogIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:5000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password}),
+    });
+
+    if(response.ok){
+        navigate('/profile', { state: { username } });
+    }else{
+        alert('ログインに失敗しました');
+    }
+  };
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>ユーザーネーム</label>
+          <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+        </div>
+      <button type="submit">ログイン</button>
+      </form>
+      <button onClick={() => navigate('/signup')}>新規登録の方はこちら</button>
+    </div>
+  );
+}
+
+export default LogIn;
--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -10,42 +10,55 @@ function SignUp() {
     //ページの遷移のために絶対にいる
     e.preventDefault();
 
-    // フォームデータをバックエンドに送信
+    //フォームデータをバックエンドに送信
     const response = await fetch('http://localhost:5000/api/register', {
+      //データ送信のためにPOSTメソッド使用　POSTリクエストを送信
       method: 'POST',
+      //リクエストのコンテキストをサーバーに送信
       headers: {
+        //リクエストのコンテンツタイプをjsonに指定
         'Content-Type': 'application/json',
       },
+      //usernameとpasswordを含むオブジェクトをjson文字列に変換する
       body: JSON.stringify({ username, password }),
     });
 
+    //レスポンスが200~299ならtrue
     if (response.ok) {
+      //指定されたパス/profifeにナビゲート(遷移)する
       navigate('/profile', { state: { username } });
     } else {
+      //メッセージを表示
       alert('登録に失敗しました');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>ユーザーネーム</label>
-        <input
-          type="text"
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>パスワード</label>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">会員登録</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>ユーザーネーム</label>
+          <input
+            type="text"
+            //入力内容をusernameのステートにセット
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            //入力内容を隠す
+            type="password"
+            //入力内容をpasswordのステートにセット
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">会員登録</button>
+      </form>
+      <button onClick={() => navigate('/')}>ログイン画面に戻る</button>
+    </div>
   );
 }
 
